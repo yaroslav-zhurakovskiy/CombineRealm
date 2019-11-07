3b0732613ff06806501501348d29ec42c19c2100
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2019-11-07 14:53:37 +0200

    Add Swift Package Manager support.

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "CombineRealm",
+    platforms: [
+        .iOS("13.0"),
+    ],
+    products: [
+        .library(name: "CombineRealm", targets: ["CombineRealm"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/realm/realm-cocoa.git",
+            from: "3.0.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "CombineRealm",
+            dependencies: [],
+            path: "CombineRealm"
+        ),
+        .testTarget(
+            name: "CombineRealmTests",
+            dependencies: ["CombineRealm"],
+            path: "CombineRealmTests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
